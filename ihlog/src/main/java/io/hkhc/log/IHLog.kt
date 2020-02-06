b20007915310f92dc193ce635b538c28873515e4
Herman Cheung <herman.kh.cheung@gmail.com>
2020-02-07 02:16:28 +0800

    Add IHLog

---
+++ ihlog/src/main/java/io/hkhc/log/IHLog.kt
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2019. Herman Cheung
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+
+package io.hkhc.log
+
+@Suppress("TooManyFunctions")
+interface IHLog {
+
+    /* IHLog class are required to implemented the following */
+
+    fun exception(tag: String?, message: String?, throwable: Throwable)
+
+    /**
+     * Implement the provider specific way for logging.
+     * @param priority - Priority of log
+     * @param tag - optional tag string. If it is null, the provider shall use its own default tag
+     * @param message - the log message
+     */
+    fun log(priority: Priority, tag: String?, message: String)
+    fun log(priority: Priority, tag: String?, lambda: () -> String) = log(priority, tag, lambda())
+
+    fun getLogTag(): String
+
+    /* helpers */
+
+    fun debug(tag: String, message: String) = log(Priority.Debug, tag, message)
+    fun debug(message: String) = log(Priority.Debug, null, message)
+    fun debug(tag: String, lambda: () -> String) = log(Priority.Debug, tag, lambda)
+    fun debug(lambda: () -> String) = log(Priority.Debug, null, lambda)
+
+    fun info(tag: String, message: String) = log(Priority.Info, tag, message)
+    fun info(message: String) = log(Priority.Info, null, message)
+    fun info(tag: String, lambda: () -> String) = log(Priority.Info, tag, lambda)
+    fun info(lambda: () -> String) = log(Priority.Info, null, lambda)
+
+    fun warn(tag: String, message: String) = log(Priority.Warn, tag, message)
+    fun warn(message: String) = log(Priority.Warn, null, message)
+    fun warn(tag: String, lambda: () -> String) = log(Priority.Warn, tag, lambda)
+    fun warn(lambda: () -> String) = log(Priority.Warn, null, lambda)
+
+    fun trace(tag: String, message: String) = log(Priority.Trace, tag, message)
+    fun trace(message: String) = log(Priority.Trace, null, message)
+    fun trace(tag: String, lambda: () -> String) = log(Priority.Trace, tag, lambda)
+    fun trace(lambda: () -> String) = log(Priority.Trace, null, lambda)
+
+    fun fatal(tag: String, message: String) = log(Priority.Fatal, tag, message)
+    fun fatal(message: String) = log(Priority.Fatal, null, message)
+    fun fatal(tag: String, lambda: () -> String) = log(Priority.Fatal, tag, lambda)
+    fun fatal(lambda: () -> String) = log(Priority.Fatal, null, lambda)
+
+    fun err(tag: String, message: String) = log(Priority.Error, tag, message)
+    fun err(message: String) = log(Priority.Error, null, message)
+    fun err(tag: String, lambda: () -> String) = log(Priority.Error, tag, lambda)
+    fun err(lambda: () -> String) = log(Priority.Error, null, lambda)
+    fun err(tag: String, throwable: Throwable) = exception(tag, null, throwable)
+}
---
+++ ihlog/src/main/java/io/hkhc/log/internal/TagMaker.kt
@@ -0,0 +1,117 @@
+/*
+ * Copyright (c) 2019. Herman Cheung
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+
+package io.hkhc.log.internal
+
+import io.hkhc.log.MetaTag
+import io.hkhc.log.providers.SimpleMetaTag
+import java.util.StringTokenizer
+
+object TagMaker {
+
+    var metaTagPolicy: MetaTag = SimpleMetaTag("")
+        set(value) {
+            field = value
+            mMetaTag = field.getTag()
+        }
+
+    private var mMetaTag: String = ""
+
+    val metaTag: String
+        get() = mMetaTag
+
+    fun getPackageNameAbbr(packageName: String): String {
+
+        val builder = StringBuilder()
+
+        val token = StringTokenizer(packageName, ".")
+        while (token.hasMoreTokens()) {
+            val t = token.nextToken()
+            builder.append(t[0].toUpperCase())
+        }
+
+        return builder.toString()
+    }
+
+    /**
+     * Given a class object, create a abbreviate string that represent it.
+     * 1. package name is ignored
+     * 2. take all capital letters and digits
+     * 3. if there are less than two capital letters, the letter after those capital letters will
+     * also be included. For example,
+     * `
+     * getClassNameAbbr("OrangeBananaApple").equals("OBA")
+     * getClassNameAbbr("OrangeBanana").equals("OrBa")
+     * getClassNameAbbr("AOrange").equals("AOr")
+     *
+     * @param className class name to be abbreviated
+     * @return abbreviated string
+     */
+    fun getClassNameAbbr(className: String): String {
+
+        val name = className
+                .substringAfterLast(".")
+                .substringBefore("\$Companion")
+                .substringBefore("\$\$")
+
+        // TODO room to optimize: reduce number of stringbuilder
+        // Shortgroup can only have at most char. So it can be a char array
+        val longBuilder = StringBuilder()
+        val shortBuilder = StringBuilder()
+
+        val nameArray = name.toCharArray()
+        nameArray[0] = nameArray[0].toUpperCase() // always treat the first as capital
+        var isLastCap = false
+        var shortGroup = 0
+        for (c in nameArray) {
+            val isCap = Character.isUpperCase(c) || Character.isDigit(c)
+            // long version
+            if (isCap) {
+                longBuilder.append(c)
+            }
+            // short version
+            if (shortGroup <= 2) {
+                if (isCap) {
+                    shortBuilder.append(c)
+                    shortGroup++
+                } else if (isLastCap) {
+                    shortBuilder.append(c)
+                }
+            }
+
+            isLastCap = isCap
+        }
+
+        return if (shortGroup > 2) {
+            longBuilder.toString()
+        } else {
+            shortBuilder.toString()
+        }
+    }
+
+    /**
+     * Create log tag from class name by abbreviation, and trim the result tag name
+     * to 23 characters max. It is also the tag size limitation to Android Log.
+     * @param clazz
+     * @return
+     */
+    fun getLogTag(clazz: Class<*>): String {
+        val delimiter = if (metaTag == "") "" else "_"
+        return "${metaTag}${delimiter}${getClassNameAbbr(clazz.name)}"
+    }
+}
